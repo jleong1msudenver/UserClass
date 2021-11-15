e3c2f4b702fbb6ab4556403ce49264a016adaafd
jleong1msudenver <jleong1@msudenver.edu>
2021-11-14 23:08:46 -0500

    getter and setter functioning properly due to newUser.getFirstName() and then newUser.setFirstName("Ron")

--- my_user.py
+++ my_user.py
@@ -3,5 +3,6 @@ from user import User
 newUser = User("Josh", "Leong", "myemail@gmail.com", 35, "303-333-555")
 print(newUser.DescribeUser())
 
-newUser.setFirstName = "Ron"
+newUser.getFirstName()
+newUser.setFirstName("Ron")
 print(newUser.DescribeUser())
