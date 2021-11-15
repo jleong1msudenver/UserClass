8aabf18ab86108747ea1c0a924f404b0a84e7648
jleong1msudenver <jleong1@msudenver.edu>
2021-11-14 23:19:28 -0500

    added jones.py object instance

---
+++ jones.py
@@ -0,0 +1,4 @@
+from user import User
+
+newUser = User("Lillian", "Jones", "ljones@myemail.com", 36, "303-333-555")
+print(newUser.DescribeUser())
--- my_user.py
+++
@@ -1,4 +0,0 @@
-from user import User
-
-newUser = User("Josh", "Leong", "myemail@gmail.com", 35, "303-333-555")
-print(newUser.DescribeUser())
